78fc269c099ec990aec28790ed15059824df40c6
Mohammed <mohammedemad557@yahoo.com>
2026-04-25 14:48:50 +0300

    carousel grid view tap bar bottom nav bar

--- lib/core/theme/appcolors.dart
+++ lib/core/theme/appcolors.dart
@@ -5,4 +5,5 @@ class AppColors{
   static Color white = Color(0xffFFFFFF);
   static Color grey = Color(0xff3A3F47);
   static Color grey2 = Color(0xff67686D);
+  static Color blue = Color(0xff0296E5);
 }
