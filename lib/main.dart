dc4f2f8a1e25b6b267b38ba1031a793b62cff29e
AHMEDSAID9485 <ahmedascahmed@gmail.com>
2026-05-02 09:33:59 +0300

    final home page and call 5 endpoint in all tap and list view hor and add search page , details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app_march26/details_screen/presentation/controllers/details_cubit.dart';
-import 'package:movie_app_march26/search/presentation/controllers/search_cubit.dart';
-import 'package:movie_app_march26/splash/presentation/splash_screen.dart';
+import 'package:movie_app_march26/Splash/presentation/SplashScreen.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MovieApp());
 }
+// {}
+// ctrl + shift + -
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => DetailsCubit(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
-        home: SplashScreen(),
-      ),
-    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splashscreen()
+      );
   }
 }
-
-
-
-
